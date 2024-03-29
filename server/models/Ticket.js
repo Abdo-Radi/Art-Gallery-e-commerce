92cb8432d9aede43889432d2fe771d2ad36a2f00
Abdellah Radi <abdellahradi5@gmail.com>
2024-03-29 14:18:01 +0000

    Task T4 :zap:

---
+++ server/models/Ticket.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const ticketSchema = new mongoose.Schema({
+    exhibitionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Exhibition"
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("Ticket", ticketSchema);
