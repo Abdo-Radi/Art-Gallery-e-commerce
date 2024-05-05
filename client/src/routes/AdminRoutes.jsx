352d6ceec984571153406ad6736b727b6f1a121b
Nisssay <yasslj06@gmail.com>
2024-05-05 11:12:26 +0000

    merging my code with mohammed's

--- client/src/routes/AdminRoutes.jsx
+++ client/src/routes/AdminRoutes.jsx
@@ -6,22 +6,28 @@ import AdminLayout from "../layout/AdminLayout"
 import SignIn from "../pages/admin/SignIn"
 import Category from "../pages/admin/Category"
 import Artwork from "../pages/admin/Artwork"
+import Exhibition from "../pages/admin/Exhibition";
+import Order from "../pages/admin/Order";
+import Ticket from "../pages/admin/Ticket";
 
 const AdminRoutes = () => {
 
     return (
-        <Routes>
-            <Route path="/admin/login" element={<SignIn />} />
-            <Route element={<ProtectedA />}>
-                <Route path="/admin" element={<AdminLayout />}>
-                    <Route index element={<Dashboard />} />
-                    <Route path="artists" element={<Artist />} />
-                    <Route path="categories" element={<Category />} />
-                    <Route path="artworks" element={<Artwork />} />
-                </Route>
-            </Route>
-        </Routes>
-    )
+      <Routes>
+        <Route path="/admin/login" element={<SignIn />} />
+        <Route element={<ProtectedA />}>
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="artists" element={<Artist />} />
+            <Route path="categories" element={<Category />} />
+            <Route path="artworks" element={<Artwork />} />
+            <Route path="exhibitions" element={<Exhibition />} />
+            <Route path="orders" element={<Order />} />
+            <Route path="tickets" element={<Ticket />} />
+          </Route>
+        </Route>
+      </Routes>
+    );
 }
 
 export default AdminRoutes
