576ed45dee2f7348e8b3768b127136c00d62b7b5
Abdellah Radi <abdellahradi5@gmail.com>
2024-05-27 14:44:21 +0100

    about landing page

--- front-office/src/routes/index.jsx
+++ front-office/src/routes/index.jsx
@@ -1,5 +1,6 @@
 import Login from "@/components/Login";
 import DefaultLayout from "@/layout/DefaultLayou";
+import About from "@/pages/About";
 import Artworks from "@/pages/Artworks";
 import Home from "@/pages/Home";
 import { Route, Routes } from "react-router-dom";
@@ -11,6 +12,7 @@ const ConfigRoutes = () => {
       <Route path="/" element={<DefaultLayout />}>
         <Route index element={<Home />} />
         <Route path="/artworks" element={<Artworks />} />
+        <Route path="/about" element={<About />} />
       </Route>
     </Routes>
   );
