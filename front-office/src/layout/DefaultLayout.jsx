00ef0ff334b5e606413bc5f1c9b1fdba289c4bcf
Nisssay <yasslj06@gmail.com>
2024-06-04 21:39:46 +0100

    chatbot

--- front-office/src/layout/DefaultLayout.jsx
+++ front-office/src/layout/DefaultLayout.jsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Chatbot from "@/components/chatBot/Chatbot"
 import { Outlet } from "react-router-dom";
 
 const DefaultLayout = () => {
@@ -7,6 +8,7 @@ const DefaultLayout = () => {
     <div className="h-screen flex flex-col">
       <Header />
       <div className="flex-grow">
+        <Chatbot />
         <Outlet />
       </div>
       <Footer />
